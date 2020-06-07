200b5444f659d8d30a3b21af00fb13ad26b7e84b
Smccoy3 <shawnmccoy@hotmail.com>
2020-06-07 17:16:16 -0400

    Added Student Tutoring assignment

---
+++ MIS500_Hypothesis Testing_t-test_Student Tutoring.R
@@ -0,0 +1,39 @@
+# R Hypothesis Tests - Student Tutoring Effects
+
+# 1. packages
+install.packages("dplyr")
+library(dplyr)
+
+# 2. Variables
+tScore_before <- c(40, 62, 74, 22, 64, 65, 49, 49, 49)
+tScore_after <- c(68, 61, 64, 76, 90, 75, 66, 60, 63)
+
+# 3. Create a data frame
+my_data <- data.frame(
+  group = rep(c("Score Before", "Score After"), each = 9),
+  scores = c(tScore_before,  tScore_after)
+)
+
+# 4. Print all data
+print(my_data)
+
+# 5. Data Manipulation
+group_by(my_data, group) %>%
+  summarise(
+    count = n(),
+    mean = mean(scores, na.rm = TRUE),
+    sd = sd(scores, na.rm = TRUE)
+  )
+
+# 6. Test statistics
+# Compute Unpaired Two Sample t-test
+res <- t.test(tScore_before, tScore_after, var.equal = TRUE)
+res
+
+# Compute independent t-test
+res <- t.test(scores ~ group, data = my_data, var.equal = TRUE)
+res
+
+#test whether the average score before score is less than the average after score, type this:
+t.test(scores ~ group, data = my_data,
+       var.equal = TRUE, alternative = "less")
